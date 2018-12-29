78ef37136a0f572cae01efb78d64fd0e30e31bff
kongaman <christian.kranz@gmail.com>
2018-12-29 17:18:40 +0100

    added a wait function

--- scrapeExample.js
+++ scrapeExample.js
@@ -6,6 +6,8 @@ var driver = new webdriver.Builder()
 
 driver.get('file://' + __dirname + '/Example.html');
 
+pause(2, scrapeExample);
+
 function scrapeExample() {
 
     //statement
@@ -13,6 +15,15 @@ function scrapeExample() {
 
 }
 
+function pause(time, funcName){         // wait before doing function
+    setTimeout(funcName, time * 1000);  // delay time in seconds
+    /*
+    * Example would be
+    * Pause(2, scrapeExample);
+    * ==> calls the metod after 2 seconds of delay
+    */
+
+}
 
 function quitDriver() {
     driver.close();
