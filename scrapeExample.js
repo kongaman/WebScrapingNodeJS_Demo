83728e8f683cec3352e1e61c6aad25044a2229ea
kongaman <christian.kranz@gmail.com>
2018-12-29 23:09:01 +0100

    bla

--- scrapeExample.js
+++ scrapeExample.js
@@ -14,9 +14,34 @@ pause(2, scrapeExample);
 
 function scrapeExample() {
 
-    console.log('Scraping the page...')
+    console.log('Scraping the page...');
+    // 1. Find Textfield Element by its id and fill it
+    driver.findElement(By.id('name')).sendKeys("Chris Scrapegoat")
+    console.log('namefield filled...');
+    // 2. Find the "female"-Radiobutton by its xpath and click on the field
+    driver.findElement(By.xpath('//input[@value="female"]')).click();
+    console.log('gender set to female...');
+    // 3. Find Checkbox by its name click on it
+    driver.findElement(By.name('vehicle1')).click();
+    console.log('Honda checked...');
+    // 4. Find unordered list by its id and loop through it
+    driver.findElements(By.id('fruits')).then(function(elements) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].getText().then(function(text){
+                console.log('List of elements in <ul>:\n' + text)
+            });         
+        }    
+    });
+    // 5. Find the "BMW"-selectlist-option by its xpath and click on it
+    driver.findElement(By.xpath('//select[@id="cars"]/option[@value="BMW"]')).click();
+    console.log('BMW selected...');
+    // 6. Find the link by a part of its text and log it to console
+    driver.findElement(By.partialLinkText('Jazeb'))
+    .getAttribute('href').then(function(value){
+        console.log("Link: " + value);
+    });
 
-
+    pause(4, quitDriver);
 }
 
 function pause(time, funcName){         // wait before doing function
